2f8201c4a443ee58e9f2489d77a1d9d5b02c344d
Vladyslav Petlovyi <>
2024-03-11 16:22:14 +0200

    added Clerk provider, installed all dependencies, added styles

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,24 @@
+import {ClerkProvider} from "@clerk/nextjs";
+import React from "react";
+import {Inter} from "next/font/google";
+
+export const metadata = {
+    title: 'Project Y',
+    description: 'An alternative to popular social medias'
+}
+
+const inter = Inter({subsets: ["latin"]})
+
+export default function RootLayout({
+                                       children
+} : {
+    children: React.ReactNode
+}) {
+    return <ClerkProvider>
+        <html lang="en">
+        <body className={`${inter.className} bg-dark-1 `}>
+        {children}
+        </body>
+        </html>
+    </ClerkProvider>
+}
