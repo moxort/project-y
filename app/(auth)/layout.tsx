3dbcbd90ecc28aae83294aed250db21e357362b6
Vladyslav Petlovyi <>
2024-03-11 18:38:48 +0200

    added onboarding

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,7 @@
 import {ClerkProvider} from "@clerk/nextjs";
 import React from "react";
 import {Inter} from "next/font/google";
+import "../globals.css";
 
 export const metadata = {
     title: 'Project Y',
---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const AccountProfile = () => {
+    return (
+        <div>
+            Account profile
+        </div>
+    )
+}
+
+export default AccountProfile;
