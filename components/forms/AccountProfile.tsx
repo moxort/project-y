2c382d8158690e8a36c9c2399f4282c9f317f657
Vladyslav Petlovyi <>
2024-03-11 21:00:24 +0200

    added AccountProfile hook

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,6 +1,25 @@
 "use client"
 
-const AccountProfile = () => {
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+
+interface Props {
+    user: {
+        id: string;
+        objectId: string;
+        username: string;
+        name: string;
+        bio: string;
+        image: string;
+    };
+    btnTitle: string;
+}
+
+const AccountProfile = ({user, btnTitle}: Props) => {
+
+    const form = useForm({
+        resolver: zodResolver()
+    })
     return (
         <div>
             Account profile
