1b2e7227dac3245b6d0b796cef2bc669abc64a3c
Vladyslav Petlovyi <>
2024-03-15 20:25:06 +0200

    created backend create post action

--- lib/actions/post.actions.ts
+++ lib/actions/post.actions.ts
@@ -32,3 +32,32 @@ export async function createPost({ text, author, communityId, path }: PostParams
     }
 
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20){
+    connectToDB();
+
+    //the number of posts to skip
+    const skipAmount = (pageNumber - 1 ) *pageSize
+    const postsQuery = Post.find({ parentId: { $in: [null, undefined]}})
+        .sort({createdAt: 'desc'})
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({path: 'author', model: User})
+        .populate({
+            path: 'children',
+            populate: {
+                path: 'author',
+                model: User,
+                select: "_id name parentId image"
+            }
+        })
+
+    const totalPostsCount = await Post.countDocuments({ parentId: { $in: [null, undefined]}})
+
+    const posts = await postsQuery.exec()
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext}
+
+}
