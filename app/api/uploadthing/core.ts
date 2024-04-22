47ff91caa8e5cdbc6b18200d3209f1b7ecf8e34a
Vladyslav Petlovyi <>
2024-04-22 10:25:39 +0300

    fixed some vercel errors

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -27,7 +27,7 @@ export const ourFileRouter = {
             console.log("file url", file.url);
 
             // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-            return { uploadedBy: metadata.userId };
+            // return { uploadedBy: metadata.userId };
         }),
 } satisfies FileRouter;
 
